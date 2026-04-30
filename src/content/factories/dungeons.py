1ff375e43db0257f29d05fa29c6a304fd9e62fe9
CaioOliveiraOFC <caio.oliveira2014@hotmail.com>
2026-04-30 20:14:24 -0300

    chore: atualiza rastreamento removendo todos os pycaches aninhados

--- src/content/factories/dungeons.py
+++
@@ -1,4 +0,0 @@
-from __future__ import annotations
-
-# Placeholder: geradores de masmorras/modificadores (data-driven)
-
--- src/ui/utils.py
+++
@@ -1,7 +0,0 @@
-import os
-import platform
-
-
-def clear_screen():
-    """Limpa a tela do console de forma multi-plataforma."""
-    os.system("cls" if platform.system() == "Windows" else "clear")
